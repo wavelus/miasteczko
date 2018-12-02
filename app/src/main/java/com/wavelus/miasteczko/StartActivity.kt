2af94f08d121645700f941bbc522a4842ba66e55
Łukasz <wolskiaxiom@interia.pl>
2018-12-02 13:45:24 +0100

    init

---
+++ app/src/main/java/com/wavelus/miasteczko/StartActivity.kt
@@ -0,0 +1,15 @@
+package com.wavelus.miasteczko
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class StartActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+
+    }
+
+
+}
