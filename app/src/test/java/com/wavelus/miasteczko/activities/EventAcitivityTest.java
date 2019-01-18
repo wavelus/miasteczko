7f545027ab8f6d34a5cffa22b0cb564df0f1d036
Łukasz <wolskiaxiom@interia.pl>
2019-01-18 21:47:21 +0100

    during big changes2

---
+++ app/src/test/java/com/wavelus/miasteczko/activities/EventAcitivityTest.java
@@ -0,0 +1,58 @@
+//package com.wavelus.miasteczko.activities;
+//
+//import android.app.Instrumentation;
+//import android.content.Intent;
+//import android.os.Bundle;
+//import android.support.v7.app.AppCompatActivity;
+//import android.view.Menu;
+//import android.widget.Button;
+//import com.google.firebase.FirebaseApp;
+//import com.wavelus.miasteczko.BuildConfig;
+//import static org.hamcrest.CoreMatchers.*;
+//
+//import com.wavelus.miasteczko.R;
+//import com.wavelus.miasteczko.activities.login.LoginActivity;
+//import com.wavelus.miasteczko.activities.login.LoginEmailActivity;
+//import junit.framework.Assert;
+////import org.hamcrest.CoreMatchers;
+//import org.junit.Before;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.robolectric.Robolectric;
+////import org.robolectric.RobolectricGradleTestRunner;
+//import org.robolectric.RobolectricTestRunner;
+//import org.robolectric.RuntimeEnvironment;
+////import org.robolectric.RuntimeEnvironment;
+////import org.robolectric.annotation.Config;
+////import org.robolectric.shadows.ShadowApplication;
+////
+////import static org.hamcrest.CoreMatchers.nullValue;
+////import static org.hamcrest.core.Is.is;
+////import static org.hamcrest.core.IsNull.notNullValue;
+////import static org.junit.Assert.assertThat;
+//import static org.junit.Assert.assertEquals;
+//import static org.robolectric.Shadows.shadowOf;
+//import static org.robolectric.shadows.ShadowInstrumentation.getInstrumentation;
+//
+//@RunWith(RobolectricTestRunner.class)
+////@Config(constants = BuildConfig.class, packageName = "org.khanacademy.android", sdk = 28)
+//
+//public class EventAcitivityTest {
+//    private AppCompatActivity activity;
+//    //    private AppCompatActivity
+//
+//    @Before
+//    public void setUp(){
+//
+//        activity = Robolectric.setupActivity(EventActivity.class);
+//    }
+//
+//
+//
+////    @Test
+////    public void onCreateTest(){
+//////        assertEquals(R.layout.activity_login,);
+////        assertEquals(R.id.activity_event_root, shadowOf(activity).getContentView().getId());
+////    }
+//}
+//
---
+++ app/src/test/java/com/wavelus/miasteczko/activities/StartActivityTest.java
@@ -0,0 +1,26 @@
+//package com.wavelus.miasteczko.activities;
+//
+//import android.support.v7.app.AppCompatActivity;
+//import com.wavelus.miasteczko.R;
+//import org.junit.Before;
+//import org.junit.Test;
+//import org.robolectric.Robolectric;
+//
+//import static org.junit.Assert.assertEquals;
+//import static org.robolectric.Shadows.shadowOf;
+//
+//public class StartActivityTest {
+//    private AppCompatActivity activity;
+//    @Before
+//    public void setUp() {
+//        activity = Robolectric.setupActivity(StartActivity.class);
+//
+//
+//    }
+//    @Test
+//    public void onCreate() {
+//        assertEquals(R.id.start_activity_root, shadowOf(activity).getContentView().getId());
+//
+//    }
+//
+//}
